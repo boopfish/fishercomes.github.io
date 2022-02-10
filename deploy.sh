6355bb8730be2d6435bfefd996636dc17c8257af
fisher <fishercomes@163.com>
2022-02-10 22:19:43 +0800

    initialize

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd .vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -15,9 +15,8 @@ cd .vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+ git push -f git@github.com:fishercomes/fishercomes.github.io.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
